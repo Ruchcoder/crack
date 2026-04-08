50636501403c6befc0abb51c667a755b77a5faa3
Ruchinna <102924739+Ruchcoder@users.noreply.github.com>
2026-04-08 03:06:14 +0100

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import cv2
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageFilter
 
-st.title("Pipeline Integrity Monitoring")
+st.title("AI-Based Pipeline & Concrete Defect Detection Demo")
 
 st.write("Upload an image of a pipe or concrete surface to detect cracks.")
 
@@ -11,17 +10,14 @@ uploaded_file = st.file_uploader("Upload Image", type=["jpg","png","jpeg"])
 
 if uploaded_file is not None:
 
-    image = Image.open(uploaded_file)
-    img = np.array(image)
+    image = Image.open(uploaded_file).convert("L")
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    edges = cv2.Canny(gray, 50, 150)
+    edges = image.filter(ImageFilter.FIND_EDGES)
 
     st.subheader("Original Image")
-    st.image(image)
+    st.image(image, use_column_width=True)
 
     st.subheader("Detected Crack Patterns")
-    st.image(edges)
+    st.image(edges, use_column_width=True)
 
     st.success("Possible Surface Crack Detected")
